757c5be9c0e3587589ef012dead4aea85340c42d
maxefimov <efimov.top.academy@gmail.com>
2023-03-28 19:27:04 +0300

    Добавьте файлы проекта.

---
+++ RazorBindingPropsApp/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorBindingPropsApp
+{
+    public class Employee
+    {
+        [BindProperty(Name = "name")]
+        public string FullName { set; get; }
+        [BindProperty(Name = "age")]
+        public int TotalAge { set; get; }
+    }
+}
---
+++ RazorBindingPropsApp/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorBindingPropsApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //[BindProperty(Name = "name")]
+        //public string FullName { set; get; }
+
+        //[BindProperty(Name = "age")]
+        //public int TotalAge { set; get; }
+
+        [BindProperty(SupportsGet = true)]
+        public Employee Employee { get; set; }
+
+        public string AnswerInfo { set; get; }
+
+        public IndexModel()
+        {
+
+        }
+
+        //public void OnGet()
+        //{
+        //    AnswerInfo = "Input User Values:";
+        //}
+        //public void OnPost()
+        //{
+        //    AnswerInfo = $"Name: {Employee.Name}, Age: {Employee.Age}";
+        //}
+    }
+}
